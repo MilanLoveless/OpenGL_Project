5b5cdd17803ec795e19fe980cc45fc8597ddcf26
Milan Loveless <the_lumberjack@hotmail.com>
2012-08-27 09:12:55 -0700

    hello world
    it works!

---
+++ MilanLoveless_OpenGL/MilanLoveless_OpenGL/GameCore.h
@@ -0,0 +1,9 @@
+// GameCore.h
+//////////////////////////////////////
+
+#include <GLTools.h>
+#include <GLShaderManager.h>
+#include <GL\glut.h>
+
+GLBatch triangleBatch;
+GLShaderManager shaderManager;
