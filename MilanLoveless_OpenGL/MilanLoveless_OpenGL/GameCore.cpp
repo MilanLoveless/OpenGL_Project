bbad2c1c459cd296ca818359d84393d9b47c9076
Milan Loveless <milan(dot)loveless(at)gmail(dot)com>
2012-09-20 12:58:26 -0700

    Revert "I broke it"
    This reverts commit 4341c74267034e5bd1fb6b7053509bfca1660ec9.

--- MilanLoveless_OpenGL/MilanLoveless_OpenGL/GameCore.cpp
+++ MilanLoveless_OpenGL/MilanLoveless_OpenGL/GameCore.cpp
@@ -10,7 +10,7 @@ void ChangeSize(int w, int h)
 
 void SetupRC()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 	shaderManager.InitializeStockShaders();
 
 	GLfloat vVerts[] = { -0.5f, 0.0f, 0.0f,
@@ -35,40 +35,6 @@ void RenderScene(void)
 	glutSwapBuffers();
 }
 
-void SpecialKeys(int key, int x, int y)
-{
-	GLfloat stepSize= 0.25f;
-
-	GLfloat blockX = vVerts[0];
-	GLfloat blockY = vVerts[7];
-
-	if(key == GLUT_KEY_UP) blockY += stepSize;
-	if(key == GLUT_KEY_DOWN) blockY -= stepSize;
-	if(key == GLUT_KEY_LEFT) blockX -= stepSize;
-	if(key == GLUT_KEY_RIGHT) blockX += stepSize;
-
-	if(blockX < -1.0f) blockX = -1.0f;
-	if(blockX > (1.0f - blockSize * 2)) blockX = 1.0f - blockSize * 2;
-	if(blockY > 1.0f) blockY = 1.0f;
-	if(blockY < (-1.0f - blockSize * 2)) blockY = -1.0f - blockSize * 2;
-
-	vVerts[0] = blockX;
-	vVerts[1] = blockY - blockSize*2;
-	
-	vVerts[3] = blockX + blockSize*2;
-	vVerts[4] = blockY - blockSize*2;
-
-	vVerts[6] = blockX + blockSize*2;
-	vVerts[7] = blockY;
-
-	vVerts[9] = blockX;
-	vVerts[10] = blockY;
-
-	squareBatch.copyVertexData3f(vVerts);
-
-	glutPostRedisplay();
-}
-
 int main(int argc, char *argv[])
 {
 	gltSetWorkingDirectory(argv[0]);
@@ -79,7 +45,6 @@ int main(int argc, char *argv[])
 	glutCreateWindow("Milan Loveless OpenGL");
 	glutReshapeFunc(ChangeSize);
 	glutDisplayFunc(RenderScene);
-	glutSpecialFunc(SpecialKeys);
 
 	GLenum err = glewInit();
 	if (GLEW_OK != err)
